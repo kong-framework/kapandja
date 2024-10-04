494f35f1daad1e9fd35f59b95565fdfdff1cf3d2
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-10-04 11:27:41 +0200

    Fix widget export

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -12,19 +12,9 @@ pub trait Widget {
     }
 
     fn export(&self) -> (String, String, String) {
-        let mut html = "".to_string();
-        let mut css = "".to_string();
-        let mut js = "".to_string();
-
-        let h: String = (self.html().clone()).to_string();
-        html = format!("out{}", h);
-
-        let c: String = (self.css().clone()).to_string();
-        css = format!("out{}", c);
-
-        let j: String = (self.js().clone()).to_string();
-        js = format!("out{}", j);
-
+        let html = self.html();
+        let css = self.css();
+        let js = self.js();
         (html, css, js)
     }
 }
