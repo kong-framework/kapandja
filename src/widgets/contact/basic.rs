f3f9f0c867b97fa38a2dfa18754c18b04f983e26
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-10-29 16:19:33 +0200

    fix basic contact widget

--- src/widgets/contact/basic.rs
+++ src/widgets/contact/basic.rs
@@ -5,34 +5,28 @@ use maud::html;
 pub struct ContactWidget {
     pub background_color: Color,
     pub phone: String,
-    pub website: String,
     pub email: String,
-    pub postal: String,
+    pub location: String,
 }
 
 impl Widget for ContactWidget {
     fn html(&self) -> String {
         html!(section #ContactWidget {
             div .columns{
-                div .col-3.col-sm-12.contact-containers{
-                    h4{i .icon.icon-apps{}}
+                div .col-4.col-sm-12.contact-containers{
+                    h4{i .fa.fa-phone{}}
                     h5{"Phone"}
                     h5 {(self.phone)}
                 }
-                div .col-3.col-sm-12.contact-containers{
-                    h4{i .icon.icon-search{}}
-                    h5{"Website"}
-                    h5 {(self.website)}
-                }
-                div .col-3.col-sm-12.contact-containers{
+                div .col-4.col-sm-12.contact-containers{
                     h4{i .icon.icon-mail{}}
                     h5{"Email"}
                     h5 {(self.email)}
                 }
-                div .col-3.col-sm-12.contact-containers{
-                    h4{i .icon.icon-edit{}}
-                    h5{"Postal"}
-                    h5 {(self.postal)}
+                div .col-4.col-sm-12.contact-containers{
+                    h4{i .fa.fa-map-marker{}}
+                    h5{"Location"}
+                    h5 {(self.location)}
                 }
             }
         })
